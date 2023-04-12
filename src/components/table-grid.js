ba4772758af46337d85bf0da145eb72cb1839eba
AnchorI <yahorkakiselevus@gmail.com>
2023-04-12 17:15:27 +0300

    nasral i tyt

--- src/components/table-grid.js
+++ src/components/table-grid.js
@@ -15,6 +15,7 @@ import 'ag-grid-community/styles/ag-theme-alpine.css'
 
 const TableGrid = ({ tableName, fullrow, index, tables, setTables, id }) => {
     const [table, setTable] = useState({})
+    const [selectedRow, setSelectedRow] = useState(null);
     const { tableUpdate } = useTableList({
         onSuccess: (data) => {
             setTable({
@@ -42,10 +43,22 @@ const TableGrid = ({ tableName, fullrow, index, tables, setTables, id }) => {
     })
 
     function handleRowClicked(event) {
-        setTables([
-            ...tables,
-            { name: 'users', fullrow: false, id: event.data.id },
-        ])
+        const tableIndex = tables.findIndex((table) => table.name === "user");
+        if (tableIndex !== -1) {
+            const updatedTable = Object.assign({}, tables[tableIndex], {
+                fullrow: false,
+                id: event.data.id,
+            });
+            const updatedTables = tables.slice();
+            updatedTables.splice(tableIndex, 1, updatedTable);
+            setTables(updatedTables);
+        } else {
+            setTables([
+                ...tables,
+                { name: "user", fullrow: false, id: event.data.id },
+            ]);
+        }
+        setSelectedRow(event.data);
     }
 
     useEffect(() => {
@@ -53,54 +66,59 @@ const TableGrid = ({ tableName, fullrow, index, tables, setTables, id }) => {
     }, [])
 
     return (
-        <div
-            className="ag-theme-alpine"
-            style={{
-                height: 400,
-                width: fullrow ? '98%' : '48%',
-                marginLeft: '1%',
-                marginRight: '1%',
-                marginBottom: 100,
-            }}
-        >
-            <Divider>
-                {tableName}
-                <Button
-                    style={{ marginLeft: '10vh' }}
-                    icon={<DeleteOutlined />}
-                    onClick={() => {
-                        const arr = [...tables]
-                        arr.splice(index, 1)
-                        setTables(arr)
-                    }}
-                    type={'primary'}
-                    danger
-                />
-            </Divider>
-            <AgGridReact
-                defaultColDef={table.defaultColDef}
-                rowData={table.data}
-                columnDefs={table.column}
-                onRowClicked={handleRowClicked}
-            />
-            <ButtonRow>
-                <Button
-                    icon={
-                        fullrow ? (
-                            <FullscreenExitOutlined />
-                        ) : (
-                            <FullscreenOutlined />
-                        )
-                    }
-                    onClick={() => {
-                        const updatedObject = [...tables]
-                        updatedObject[index].fullrow =
-                            !updatedObject[index].fullrow
-                        setTables(updatedObject)
+            <div
+                className="ag-theme-alpine"
+                style={{
+                    height: 400,
+                    width: fullrow ? '98%' : '48%',
+                    marginLeft: '1%',
+                    marginRight: '1%',
+                    marginBottom: 100,
+                }}
+            >
+                <Divider>
+                    {tableName}
+                    <Button
+                        style={{ marginLeft: '10vh' }}
+                        icon={<DeleteOutlined />}
+                        onClick={() => {
+                            const arr = [...tables]
+                            arr.splice(index, 1)
+                            setTables(arr)
+                        }}
+                        type={'primary'}
+                        danger
+                    />
+                </Divider>
+                <AgGridReact
+                    defaultColDef={table.defaultColDef}
+                    rowData={table.data}
+                    columnDefs={table.column}
+                    onRowClicked={handleRowClicked}
+                    rowSelection='single'
+                    suppressRowClickSelection={true}
+                    getRowClass={(params) => {
+                        return selectedRow && selectedRow.id === params.data.id ? 'selected' : '';
                     }}
                 />
-            </ButtonRow>
-        </div>
+                <ButtonRow>
+                    <Button
+                        icon={
+                            fullrow ? (
+                                <FullscreenExitOutlined />
+                            ) : (
+                                <FullscreenOutlined />
+                            )
+                        }
+                        onClick={() => {
+                            const updatedObject = [...tables]
+                            updatedObject[index].fullrow =
+                                !updatedObject[index].fullrow
+                            setTables(updatedObject)
+                        }}
+                    />
+                </ButtonRow>
+            </div>
     )
 }
 
